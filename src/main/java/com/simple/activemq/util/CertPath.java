946d17acab2ec8c74d48fb6364e46e70da39a3dd
rahul95sonii <rahul95sonii@gmail.com>
2022-04-13 20:57:48 +0530

    Read the SSL Certificate from Specific folder

---
+++ src/main/java/com/simple/activemq/util/CertPath.java
@@ -0,0 +1,35 @@
+package com.simple.activemq.util;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import java.security.*;
+
+public class CertPath {
+    public static SSLContext createSSLContext(String resourcePath, String pass) throws NoSuchAlgorithmException{
+        SSLContext sslc = SSLContext.getInstance("TLS");
+        try {
+// initialise the keystore
+            final char[] password = pass.toCharArray();
+            KeyStore ks = KeyStore.getInstance("JKS");
+            ks.load(com.simple.activemq.pathCert.class.getResourceAsStream(resourcePath), password);
+// Setup the key manager factory.
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init(ks, password);
+// Setup the trust manager factory.
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+            tmf.init(ks);
+// sslc = SSLContext.getInstance("TLS");
+            sslc.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+// return sslc;
+        }catch(Exception se){
+            se.printStackTrace();
+        }
+        return sslc;
+    }
+    public static void main(String[] args) {
+        try {
+            SSLContext.setDefault(
+                    createSSLContext("/resource_path", "Password"));
+        }catch(Exception se){se.printStackTrace();}
+    }
+}
